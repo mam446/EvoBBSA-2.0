c2f5e50feb2b1d6cc210d5a18f8e582138d9a9ec
Matt Martin <mam446@mst.edu>
2013-09-03 22:58:24 -0500

    added setup file

---
+++ setup.py
@@ -0,0 +1,22 @@
+
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+setup(
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = [Extension("bbsa",["bbsa.pyx"]),
+                   Extension("evalNodes",["evalNodes.pyx"]),
+                   Extension("fitness",["fitness.pyx"]),
+                   Extension("genNode",["genNode.pyx"]),
+                   Extension("logger",["logger.pyx"]),
+                   Extension("selectNodes",["selectNodes.pyx"]),
+                   Extension("setNodes",["setNodes.pyx"]),
+                   Extension("settings",["settings.pyx"]),
+                   Extension("solution",["solution.pyx"]),
+                   Extension("termNodes",["termNodes.pyx"]),
+                   Extension("variationNodes",["variationNodes.pyx"]),
+                   Extension("funcs", ["funcs.pyx"]),
+                   Extension("representations", ["representations.pyx"]),
+                   Extension("state",["state.pyx"])]
+)
