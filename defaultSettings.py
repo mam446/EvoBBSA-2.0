bf1a19837fc6c22cce3aca57b90212d4e25317bd
Matt Martin <mam446@mail.mst.edu>
2014-09-07 22:57:43 -0500

    I added a ton of stuff
    -there is now a conditional statement that is based on convergence. If it has converged
    	so long it will do something different.
    
    -I created a hydra.py program that will run a config file in a specific directory for you so you
    	can run it from the main directory and all the files end up somewhere else.
    
    	It runs a specific program with or without mpi depending on the settings file that you give
    	it. if you need help with it just come talk to me
    
    -I fixed labels for the plots.
    
    -I am pretty sure I fixed how rng seeds work
    
    -I took out all of the stupid logging stuff from the mpi programs
    
    TODO:
    
    I need to add functionality for hydra.py to run on multiple machines.
    
    I need to change how the dominate function works in bbsa to allow for more flexibility
    
    I need to make a new run.py program that works easier
    
    If I can i want to inbed git information into the output files to track program versions
    
    Bad Joke:
    
    Which food can you eat off of?
    
    A vege-table.

---
+++ defaultSettings.py
@@ -0,0 +1,194 @@
+
+
+
+
+{
+    'hyperSettings':
+    {
+        'runName':"testRun",
+        'runNumber':0,
+        'mu':100,
+        'lambda':40,
+        'type':'SGA',#this can be SGA or NSGA
+        'mpi':True,
+        'procs':5,
+        'mateRate':.3,
+        'mutateRate':.3,
+        'evaluations':5000,
+        'objectives':['evals','fitness','time'],
+        'childK':8,
+        'seed':None
+    },
+
+
+    'bbsaSettings':
+    {
+        'maxStartNodes':15,
+        'maxEvals':100000,
+        'maxOps':5000000,
+        'maxIterations':10000,
+        'maxDepth':5,
+        'mutateMax':5,
+        'runs':5,
+        'converge':30,
+        'initPopMax':50,
+        'probType':'bitString'
+    },
+    
+    'problems':
+    [
+        {
+            'weight':1,
+            'repr':'bitString',
+            'prob':'dTrap',
+            'settings':
+            {
+                'length':100,
+                'k':5
+            }
+        },
+
+        {
+            'weight':1,
+            'repr':'bitString',
+            'prob':'dTrap',
+            'settings':
+            {
+                'length':210,
+                'k':7
+            }
+        },
+
+
+    ],
+    
+    
+    'nodeSettings':
+    {
+        'mutate':
+        {
+            'rate':
+            {
+                'value':0.0,
+                'range':(0.0,1.0),
+                'type':'float'
+            },
+            'weight':2
+        },
+        'gaussian':
+        {
+            'rate':
+            {
+                'value':0.0,
+                'range':(0.0,100.0),
+                'type':'float'
+            },
+            'variance':
+            {
+                'value':0.0,
+                'range':(0.0,1.0),
+                'type':'float'
+            },
+            'weight':2
+        },
+        'uniRecomb':
+        {
+            'num':
+            {
+                'value':1,
+                'range':(1,25),
+                'type':'int'
+            },
+            'weight':2
+        },
+        'diagonal':
+        {
+            'n':
+            {
+                'value':1,
+                'range':(1,25),
+                'type':'int'
+            },
+            'weight':2
+        },
+        'onePoint':
+        {
+            'weight':2
+        },
+        'kTourn':
+        {
+            'k':
+            {
+                'value':1,
+                'range':(1,25),
+                'type':'int'
+            },
+            'count':
+            {
+                'value':1,
+                'range':(1,25),
+                'type':'int'
+            },
+            'weight':2
+        },
+        'trunc':
+        {
+            'count':
+            {
+                'value':1,
+                'range':(1,25),
+                'type':'int'
+            },
+            'weight':2
+        },
+        'randSubset':
+        {
+            'count':
+            {
+                'value':1,
+                'range':(1,25),
+                'type':'int'
+            },
+            'weight':2
+        },
+        'forLoop':
+        {
+            'count':
+            {
+                'value':1,
+                'range':(1,25),
+                'type':'int'
+            },
+            'weight':2
+        },
+        'ifConverge':
+        {
+            'conv':
+            {
+                'value':5,
+                'range':(5,25),
+                'type':'int'
+            },
+            'weight':2
+        },
+        'makeSet':
+        {
+            'name':
+            {
+                'value':"",
+                'type':'str'
+            },
+            'weight':2
+        }
+
+    },
+     
+
+}
+
+
+
+
+
+
+
